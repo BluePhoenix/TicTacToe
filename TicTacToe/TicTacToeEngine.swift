564f1827ec072ed00152b7ab11c5c2867013aafc
Felix Barros <felix.barros@gmail.com>
2015-11-01 21:01:49 -0500

    Create baseline of TicTacToeEngine and some test coverage

---
+++ TicTacToe/TicTacToeEngine.swift
@@ -0,0 +1,33 @@
+//
+//  TicTacToeEngine.swift
+//  TicTacToe
+//
+//  Created by Felix Barros on 11/1/15.
+//  Copyright © 2015 Bits That Matter. All rights reserved.
+//
+
+import Foundation
+
+
+// Since I am learning, I am implementing all this myself
+// However, a good alternative might be https://github.com/scottsievert/swix
+class TicTacToeEngine {
+    var rawData: Array<Array<Int>>
+    var currentPlayer = 0
+    
+    func play(x: Int, y: Int) {
+        // Change data
+        rawData[x][y] = (currentPlayer + 1)
+        
+        // Change player turn
+        currentPlayer = (currentPlayer+1) % 2
+    }
+    
+    init() {
+        rawData = [[Int]]()
+        let emptyRow = [0, 0, 0]
+        for var i = 0; i < 3; i++ {
+            rawData.append(emptyRow)
+        }
+    }
+}
---
+++ TicTacToeTests/TicTacToeEngineTests.swift
@@ -0,0 +1,55 @@
+//
+//  TicTacToeEngineTests.swift
+//  TicTacToe
+//
+//  Created by Felix Barros on 11/1/15.
+//  Copyright © 2015 Bits That Matter. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class TicTacToeEngineTests: XCTestCase {
+    var ticTacToeEngine: TicTacToeEngine!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        ticTacToeEngine = TicTacToeEngine()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testDefaultStateOfEngine() {
+        let defaultRow = Array(count: 3, repeatedValue: 0)
+        let expectedData = Array(count: 3, repeatedValue: defaultRow)
+        XCTAssertEqual( ticTacToeEngine.rawData, expectedData)
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        ticTacToeEngine.play(0, y: 2)
+        var expectedData = [ [0, 0, 1], [0, 0, 0], [0, 0, 0] ]
+        XCTAssertEqual( String(ticTacToeEngine.rawData), String(expectedData) )
+
+        ticTacToeEngine.play(1, y: 2)
+        expectedData = [ [0, 0, 1], [0, 0, 2], [0, 0, 0] ]
+        XCTAssertEqual( String(ticTacToeEngine.rawData), String(expectedData) )
+        
+        ticTacToeEngine.play(2, y: 0)
+        expectedData = [ [0, 0, 1], [0, 0, 2], [1, 0, 0] ]
+        XCTAssertEqual( String(ticTacToeEngine.rawData), String(expectedData) )
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
